e67c17e57883161116e6ff4531901d528d15060b
Antonio <tonito@umich.edu>
2021-12-15 19:08:59 -0500

    HTML created

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const inquirer = require('inquirer');
-/*const fs = require ('fs');
+const fs = require ('fs');
 
 const generatePage = require('./src/page-template.js');
 
-const pageHTML = generatePage (name, github);
+/*const pageHTML = generatePage (name, github);
 
 fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw err;
@@ -137,7 +137,11 @@ const promptProject = portfolioData => {
 };
 
   promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-  });
+    .then(promptProject)
+    .then(portfolioData => {
+      const pageHTML = generatePage (portfolioData);
+
+      fs.writeFile('index.html', generatePage(portfolioData), err => {
+          if (err) throw err;
+      });
+    });
